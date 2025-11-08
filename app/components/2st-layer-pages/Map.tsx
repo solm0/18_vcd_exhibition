b9382ee4d090bff799d64a07e330a26a1239f3ce
정솔미 <solmi-@kookmin.ac.kr>
2025-11-09 08:08:03 +0900

    지도

--- app/components/2st-layer-pages/Map.tsx
+++ app/components/2st-layer-pages/Map.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { detailAxisDeg, detailTop, detailTransX, } from "@/app/lib/pageLayout";
-import { rooms } from "@/app/lib/rooms";
+import { detailAxisDeg } from "@/app/lib/pageLayout";
+import { Room, rooms } from "@/app/lib/rooms";
 import { useHoveredRoomStore } from "@/app/lib/store/useHoveredRoomStore";
 import Image from "next/image";
 import { useState } from "react";
@@ -10,19 +10,24 @@ import { Fragment } from "react/jsx-runtime";
 export default function Map() {
   const hoveredRoom = useHoveredRoomStore((state) => state.hoveredRoom);
   const setHoveredRoom = useHoveredRoomStore((state) => state.setHoveredRoom);
+  const [visilbeRooms, setVisibleRooms] = useState<Room[]>([]);
 
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0});
-  const handleMouseMove = (e: React.MouseEvent) => {
-    setMousePos({ x: e.clientX, y: e.clientY });
-  };
+  const roomWPolygon = rooms.filter(room => room.d);
+  const roomWoPolygon = rooms.filter(room => !room.d);
 
-  const polygon = (room:{id: string; n: string; title: string; d: string; href?: string | undefined}) => {
+  const polygon = (room: Room) => {
     return (
       <path
         key={room.id}
         onMouseEnter={() => setHoveredRoom(room.id)}
         onMouseLeave={() => setHoveredRoom(null)}
-        fill={ hoveredRoom === room.id ? "#0000ff" : "#ffffff"
+        onClick={() => setVisibleRooms(prev => {
+          const exists = prev.some(r => r.id === room.id);
+          return exists
+            ? prev.filter(r => r.id !== room.id)
+            : [...prev, room];
+        })}
+        fill={ hoveredRoom === room.id ? "#fafafa90" : "#d4d4d470"
         }
         d={room.d}
       >
@@ -30,26 +35,38 @@ export default function Map() {
     )
   }
 
-  const content = (room:{id: string; n: string; title: string; d: string; href?: string | undefined}) => {
+  const content = (room:Room) => {
     return (
       <div
         key={room.id}
         onMouseEnter={() => setHoveredRoom(room.id)}
         onMouseLeave={() => setHoveredRoom(null)}
         className={`
-          ${hoveredRoom === room.id ? "bg-[#0000ff]! text-gray-100" : room.href ? "text-[#0000FF] bg-white" : 'text-black bg-white'}
-          px-3 py-1 flex items-center justify-center text-sm flex-1 hover:text-background-100
+          ${hoveredRoom === room.id ? "bg-neutral-400 text-neutral-900 border-neutral-900" : 'text-neutral-200 border-neutral-200 bg-transparent'}
+          ${room.desc.length > 12 ? 'w-150' : room.desc.length > 6 ? 'w-100' : 'w-48'} h-45 px-3 py-1 text-xs hover:text-background-100
+          flex flex-col shrink-0 grow-0 items-start gap-1
         `}
       >
-        {room.title}
-        {room.href && <span>*</span>}
+        <p className="scale-x-95 tracking-normal origin-left text-base h-7 shrink-0 border-b w-full">{room.id}</p>
+        <div className={`
+          flex flex-col leading-[1.75em] h-[calc(100%-2rem)] w-full items-start justify-start
+          ${room.desc.length > 6 ? 'flex-wrap' : ''}
+        `}>
+          {room.desc.map(p => (
+            <p
+              key={p}
+              className="scale-x-95 tracking-normal origin-left"
+            >
+                {p}
+            </p>
+          ))}
+        </div>
       </div>
     )
   };
 
   return (
     <div
-      onMouseMove={handleMouseMove}
       className={`
         ${detailAxisDeg} flex items-center justify-center
         absolute overflow-hidden
@@ -58,7 +75,7 @@ export default function Map() {
         w-screen h-screen 
       `}
     >
-      <div className="relative w-full max-w-[2100px] aspect-2/1">
+      <div className="relative w-full max-w-[2100px] aspect-2/1 -translate-y-30">
         {/* 지도 이미지 */}
         <Image
           src="/map/map.svg"
@@ -77,30 +94,43 @@ export default function Map() {
           <g
             transform="
               rotate(180 3300 1100)
-              scale(1.16)
-              translate(-1450 -610)
+              scale(1.165)
+              translate(-1480 -620)
             "
           >
-            {rooms.map((room) => (
+            {roomWPolygon.map((room) => (
               <Fragment key={room.id}>{polygon(room)}</Fragment>
             ))}
           </g>
         </svg>
+
+        <div className="absolute z-30 w-full h-full pointer-events-none">
+          {roomWoPolygon.map(room => (
+            <div
+              key={room.id}
+              className={`
+                ${hoveredRoom === room.id ? "bg-[#fafafa90]" : "bg-[#d4d4d470]"}
+                rounded-full absolute w-8 h-8 flex items-center justify-center text-neutral-900
+                ${room.pos} pointer-events-auto
+              `}
+              onMouseEnter={() => setHoveredRoom(room.id)}
+              onMouseLeave={() => setHoveredRoom(null)}
+              onClick={() => setVisibleRooms(prev => {
+                const exists = prev.some(r => r.id === room.id);
+                return exists
+                  ? prev.filter(r => r.id !== room.id)
+                  : [...prev, room];
+              })}
+            >
+              {room.id.slice(-1)}
+            </div>
+          ))}
+        </div>
       </div>
 
-      {hoveredRoom &&
-        <div
-          className="fixed z-80 h-auto w-36 flex items-center px-4 py-3 bg-gray-200 text-neutral-900 text-sm top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
-          style={{
-            position: 'fixed',
-            top: mousePos.y,
-            left: mousePos.x,
-            cursor: "none"
-          }}
-        >
-          {rooms.find(room => room.id === hoveredRoom)?.title}
-        </div>
-      }
+      <div className="absolute w-3/5 h-2/5 top-1/2 left-[5%] flex flex-wrap gap-2 justify-center overflow-y-scroll custom-scrollbar">
+        {visilbeRooms.map((room) => content(room))}
+      </div>
     </div>
   )
 }
